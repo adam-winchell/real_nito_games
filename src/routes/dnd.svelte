19e8c240ef8d7d96cc1128dfdc492a4e42220cba
Adam Winchell <winchell2326adam@gmail.com>
2023-01-04 19:44:40 -0600

    changes

--- src/routes/dnd.svelte
+++ src/routes/dnd.svelte
@@ -6,44 +6,18 @@
     </p>
     <ul>
         <li>
-            <a target="_self" href="games/Extrodinary_Actions.pdf">
+            <a  href="http://www.dmsguild.com/product/422142">
                 <strong>Extraordinary Actions </strong>  written by Adam Winchell and Josh Ladd.
             </a>
         </li>
         <li>
-            <a target="_self" href="https://github.com/adam-winchell/Xem-s-Guide-To-the-Underdark-PDFs/blob/main/output/XGU_Players_Supplement.pdf">
+            <a  href="https://github.com/adam-winchell/Xem-s-Guide-To-the-Underdark-PDFs/blob/main/output/XGU_Players_Supplement.pdf">
                 <strong>Xem's Guide To the Underdark Players Supplement </strong>  written by Adam Winchell and Josh Ladd.
             </a>
         </li>
     </ul>
 </div>
-<div id="example-visualization" style='width: 100%'></div>
     </section>
-
-<svelte:head>
-    <link rel="stylesheet" href="/src/webweb/style.css">
-    <script type="text/javascript" src="src/webweb/Blob.js"></script>
-    <script type="text/javascript" src="src/webweb/colors.js"></script>
-    <script type="text/javascript" src="src/webweb/d3.v5.min.js"></script>
-    <script type="text/javascript" src="src/webweb/FileSaver.min.js"></script>
-    <script type="text/javascript">
-        // var wwdata;
-        // fetch('src/stores/viz.json')
-        // .then(response => response.json())
-        // .then(data => {wwdata = data})
-        // console.log(wwdata);
-        //TODO the above is not JSON its a string  
-    
-        var wwdata = {"display":{"attachWebwebToElementWithId":"example-visualization"},"networks":{"webweb":{"layers":[{"edgeList":[[0,1],[1,2],[2,3],[3,4],[4,5]],"metadata":null,"nodes":{}}]}},"title":"webweb"};
-        // wwdata.display.attachWebwebToElementWithId = "example-visualization";
-        wwdata.display.w = 250;
-        wwdata.display.h = 250;
-
-        //window.onload is not being called below unless developer tools open
-        </script>
-        <!-- <script type="text/javascript" src="src/webweb/webweb.v5.js?1565662230803397000"></script> -->
-        <script type="text/javascript" src="src/webweb/webweb.v5.js?1565662230803397000"></script>
-</svelte:head>
 
 
 <style>
