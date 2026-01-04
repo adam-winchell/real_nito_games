21241bc187a993d999692a232ae1456bb36e12a1
Adam Winchell <winchell2326adam@gmail.com>
2026-01-04 17:39:26 -0500

    change video

--- src/routes/coral_clash/+page.svelte
+++ src/routes/coral_clash/+page.svelte
@@ -37,8 +37,7 @@
         <p>Perfect for fans of chess, abstract strategy games, and ocean adventures!</p>
 
         <h2>Rules Video</h2>
-
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/AacHXQ6-dIA?si=ts2UbiDg0GydLQ10" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/-F9exS5vlKo?si=BlfbeYPfIQRjIwIb" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         <h2>Gameplay Video</h2>
         <iframe  src="https://youtube.com/embed/jlV2ywx9we8" title="YouTube video player" frameborder="0" 
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
